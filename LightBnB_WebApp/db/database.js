5bbfe2ec6ec0e0c4c9d0b413843fc83c47730d26
TayrineSoares <tayrinecristina@hotmail.com>
2025-01-30 09:44:55 -0300

    Refactor getUserWithEmail

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -1,6 +1,17 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+// connects to the database using node-postgres
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "lightbnb",
+});
+
+
 /// Users
 
 /**
@@ -8,16 +19,33 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
-};
+
+const getUserWithEmail = (email) => {
+  return pool
+    .query(
+      `
+      SELECT * FROM users 
+      WHERE LOWER(users.email) = LOWER($1);
+      `,
+      [email] //// Pass the email as a parameter
+    )
+
+    .then ((result) => {
+      //console.log("THIS IS MY RESULT OBJECT", result);
+      // result.rows always contains the result of your SQL query
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })    
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };
+
+
+
 
 /**
  * Get a single user from the database given their id.
@@ -59,13 +87,20 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+
+
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      //console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
 
 /**
  * Add a property to the database
